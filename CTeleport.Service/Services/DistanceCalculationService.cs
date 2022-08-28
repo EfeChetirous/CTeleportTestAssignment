77d99980f3a03c8f8268b0069530a43c8a3e69f6
Burak Çelik <bcburakcelik@gmail.com>
2022-08-29 00:18:46 +0300

    Calculation has been completed

--- CTeleport.Service/Services/DistanceCalculationService.cs
+++ CTeleport.Service/Services/DistanceCalculationService.cs
@@ -1,6 +1,7 @@
 ﻿using CTeleport.Model.ApiResultModel;
 using CTeleport.Model.Models;
 using CTeleport.Service.Interfaces;
+using Geolocation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,12 +24,24 @@ namespace CTeleport.Service.Services
                 DistanceCalculationResponseModel response = new DistanceCalculationResponseModel();
                 response.DepartureAirport = await _airportService.GetAirportInfoAsync(requestModel.DepartureAirport);
                 response.ArrivalAirport = await _airportService.GetAirportInfoAsync(requestModel.ArrivalAirport);
+                response.DistanceInMiles 
+                    = GeoCalculator.GetDistance(response.DepartureAirport.Location.Latitude, response.DepartureAirport.Location.Longitude, response.ArrivalAirport.Location.Latitude, response.ArrivalAirport.Location.Longitude, 1).ToString();
                 return new SuccessResult(response,"success"); 
             }
             catch (Exception ex)
             {
                 return new FailureResult(ex.Message);
             }
+        }
+        private double CalculateDistance(Location point1, Location point2)
+        {
+            var d1 = point1.Latitude * (Math.PI / 180.0);
+            var num1 = point1.Longitude * (Math.PI / 180.0);
+            var d2 = point2.Latitude * (Math.PI / 180.0);
+            var num2 = point2.Longitude * (Math.PI / 180.0) - num1;
+            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) +
+                     Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
+            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
         }
     }
 }
